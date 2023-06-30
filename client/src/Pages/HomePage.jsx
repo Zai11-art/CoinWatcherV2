7061f714b3efabc514188200421d6152af1e4661
Christian Samaniego <66768290+Zai11-art@users.noreply.github.com>
2023-06-30 18:34:30 +0800

    Added exchange page section
    removed file duplicates, added exchange page sections. refactored some code.

--- client/src/Pages/HomePage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const HomePage = () => {
-  return (
-    <div className='w-[100%]  h-screen bg-[#051925] flex flex-col items-center'>HomePage</div>
-  )
-}
-
-export default HomePage
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import compression from 'vite-plugin-compression';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    compression({
-      algorithm: 'gzip',
-      ext: '.gz',
-    }),
-  ],
-});
+  plugins: [react()],
+})
